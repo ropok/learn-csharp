5aa3fca2168f96b454f24141637133f55a708f8b
Jaler Sekar Maji <jalerse@gmail.com>
2021-05-04 14:18:58 +0700

    feat(if): learn if else and switch case

--- main.cs
+++ main.cs
@@ -3,13 +3,20 @@ using System;
 namespace main {
     class Program {
         static void Main(string[] args){
+            
+            int i = 13;
+            
+            switch (i) {
+                default:
+                    Console.Write("No case!");
+                    break;
+                case 1:
+                    Console.WriteLine("First case!");
+                    break;
+                case 5:
+                    break;
+            }
 
-            int i = 56;
-
-            Console.WriteLine("Hello World! " + i);
-
-            TestFunction();
-            bool b = SecondTestFunction(50);
             Console.ReadKey();
         }
 
