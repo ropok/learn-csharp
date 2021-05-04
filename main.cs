7af285c27a52d47863e43db51d68872510616a09
Jaler Sekar Maji <jalerse@gmail.com>
2021-05-04 14:36:15 +0700

    feat(comment): single and multiple comment

--- main.cs
+++ main.cs
@@ -5,7 +5,14 @@ namespace main {
     class Program {
         static void Main(string[] args){
             
-            // int[] intArray = new int[5];
+            // This is a comment
+            
+            /*
+            *
+            *Multi line comment!
+            * comment comment
+            * */
+
             int[] intArray = new int[] { 1, 2, 3, 4, 5 };
 
             List<int> intList = new List<int>() { 1, 2, 3};
