67c594e4400385811cbdb27ee8dfa1d8cfc88e46
Jaler Sekar Maji <jalerse@gmail.com>
2021-05-04 14:33:34 +0700

    feat(loop): while

--- main.cs
+++ main.cs
@@ -9,9 +9,10 @@ namespace main {
             int[] intArray = new int[] { 1, 2, 3, 4, 5 };
 
             List<int> intList = new List<int>() { 1, 2, 3};
-            
-            for (int i = 0; i < intList.Count; i++) {
-                Console.WriteLine(i + " " + intList[i]);
+
+            int i = 0;
+            while (i < 5) {
+                i++;
             }
 
             Console.ReadKey();
